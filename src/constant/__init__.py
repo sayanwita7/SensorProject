442672a0c5ac8ba11d42cd4f9a194d3ab6cd981a
sayanwita7 <dey.sayanwita7@gmail.com>
2025-07-25 00:25:59 +0530

    Fourth commit

--- src/constant/__init__.py
+++ src/constant/__init__.py
@@ -1,10 +1,15 @@
 import os
 AWS_S3_BUCKET_NAME = "wafer-fault"
-MONGO_DATABASE_NAME = "pwskills"
+MONGO_DATABASE_NAME = "SensorFaultDetection"
 MONGO_COLLECTION_NAME = "waferfault"
+
+TARGET_COLUMN = "quality"
+MONGO_DB_URL = "mongodb+srv://deysayanwita7:vhORAX6UcS6vTkDe@cluster0.iihxnaq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 TARGET_COLUMN="quality"
 MONGO_DB_URL =""
 
 MODEL_FILE_NAME="model"
 MODEL_FILE_EXTENSION=".pkl"
+
+artifact_folder = "artifacts"
---
+++ upload_data.py
@@ -0,0 +1,11 @@
+from pymongo.mongo_client import MongoClient
+import pandas as pd
+import json
+uri="mongodb+srv://deysayanwita7:vhORAX6UcS6vTkDe@cluster0.iihxnaq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+client=MongoClient(uri)
+DATABASE_NAME="SensorFaultDetection"
+COLLECTION_NAME='waferfault'
+df=pd.read_csv("C:\Users\deysa\SensorProject\notebooks\wafer_23012020_041211.csv")
+df=df.drop("Unnamed: 0",axis=1)
+json_record=list(json.loads(df.T.to_json()).values())
+client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
